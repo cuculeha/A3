db4f3f8822b290edd72681be65fab7a2e37a469a
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-16 21:20:50 -0800

    final cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,62 @@
+// Author      : Saliha Babar
+// Assignment  : A2
+// Due Date    : 01/21/2025
+// Purpose     : C-Style Arrays
+
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
+const int numBooks = 20;
+const int maxChars = 80;
+
+// Function to calculate string length safely
+size_t stringLength (const char characters [], size_t maxChars);
+
 int main(){
+  char bookTitle[numBooks][maxChars] = {"Star Wars", "The Empire Strikes Back (Star Wars)",
+										"Splinter of the Mind's Eye (Star Wars)", "Return of the Jedi (Star Wars)",
+										"Heir to the Empire (Star Wars: The Thrawn Trilogy, Vol. 1)", 
+										"Dark Force Rising (Star Wars: The Thrawn Trilogy, Vol. 2",
+										"The Last Command (Star Wars: The Thrawn Trilogy)",
+										"Specter of the Past (Star Wars: The Hand of Thrawn #1)",
+										"Vision of the Future (Star Wars: The Hand of Thrawn, Book 2)",
+										"Battlestar Galactica", "New Battlestar Galactica Volume 1 (Battlestar Galactica (Dynamite)) (v. 1)",
+										"New Battlestar Galactica Volume II Hardcover (Battlestar Galactica (Dynamite)", "The Hobbit",
+										"The Lord of the Rings: 50th Anniversary, One Vol. Edition", "The Two Towers", "The Return of the King",
+										"The Fellowship of the Ring", "C++ Programming: From Problem Analysis to Program Design 7th Edition",
+										"Digital Computer Electronics", "Assembly Language for x86 Processors (7th Edition)"};
+	int onHand [20] = {17,3,6,0,19,7,18,15,5,4,7,14,18,12,12,3,5,19,16,15};
+	
+	int maxQuantity;
+	int minQuantity;
+	int maxIndex;
+	int minIndex;
+	
+	maxQuantity = onHand[0];
+	minQuantity = onHand [0];
 
-cout << "Hello World! Adding new things from github" << endl;
+  for (int i = 1; i < numBooks ; i++)
+    {
+      if (onHand[i] > maxQuantity)
+      {
+      	maxIndex = i;
+      	maxQuantity = onHand[i];
+	  }
+	  
+	  else if (onHand[i] < minQuantity)
+	  {
+	  	minIndex = i;
+	  	minQuantity = onHand[i];
+	  }
+	  
+    }
 
-return 0;
+	// Display the output
+	cout << "Index\t" << left << left <<setw (70) << "Book Title" << "onH\n";
+	cout << "[" << minIndex<< "]\t" << left << setw (70) << bookTitle[minIndex] << minQuantity << endl;
+	cout << "[" << maxIndex<< "]\t" << left << setw (70) << bookTitle[maxIndex] << maxQuantity << endl;
+	
+	return 0;
 }
