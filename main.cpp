fcd0479f9c94335172d21c1449f6458748f7ff1b
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-20 13:29:06 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,12 +16,12 @@ int main(){
 	array < string, numBooks > bookTitle {"Star Wars", "The Empire Strikes Back (Star Wars)",
 						"Splinter of the Mind's Eye (Star Wars)", "Return of the Jedi (Star Wars)",
 						"Heir to the Empire (Star Wars: The Thrawn Trilogy, Vol. 1)", 
-						"Dark Force Rising (Star Wars: The Thrawn Trilogy, Vol. 2"},
+						"Dark Force Rising (Star Wars: The Thrawn Trilogy, Vol. 2",
 						"The Last Command (Star Wars: The Thrawn Trilogy)",
 						"Specter of the Past (Star Wars: The Hand of Thrawn #1)",
 						"Vision of the Future (Star Wars: The Hand of Thrawn, Book 2)",
 						"Battlestar Galactica", "New Battlestar Galactica Volume 1 (Battlestar Galactica (Dynamite)) (v. 1)",
-						"New Battlestar Galactica Volume II Hardcover (Battlestar Galactica (Dynamite)","The Hobbit"},
+						"New Battlestar Galactica Volume II Hardcover (Battlestar Galactica (Dynamite)","The Hobbit",
 						"The Lord of the Rings: 50th Anniversary, One Vol. Edition", "The Two Towers", "The Return of the King",
 						"The Fellowship of the Ring", "C++ Programming: From Problem Analysis to Program Design 7th Edition",
 						"Digital Computer Electronics", "Assembly Language for x86 Processors (7th Edition)"};
@@ -30,8 +30,8 @@ int main(){
 	
 	size_t maxQuantity;
 	size_t minQuantity;
-	int maxIndex;
-	int minIndex;
+	string bookWithMaxQuantity;
+	string bookWithMinQuantity;
 	
 	maxQuantity = onHand[0];
 	minQuantity = onHand [0];
@@ -40,13 +40,13 @@ int main(){
     {
       if (onHand[i] > maxQuantity)
       {
-      	maxIndex = i;
+      	bookWithMaxQuantity = i;
       	maxQuantity = onHand[i];
 	  }
 	  
 	  else if (onHand[i] < minQuantity)
 	  {
-	  	minIndex = i;
+	  	bookWithMinQuantity = i;
 	  	minQuantity = onHand[i];
 	  }
 	  
@@ -55,8 +55,8 @@ int main(){
 
 	// Display the output
 	cout << "Index\t" << left << left <<setw (70) << "Book Title" << "onH\n";
-	cout << "[" << minIndex<< "]\t" << left << setw (70) << format( "{}", bookTitle [minIndex]) << minQuantity << endl;
-	cout << "[" << maxIndex<< "]\t" << left << setw (70) << format( "{}", bookTitle [maxIndex]) << maxQuantity << endl;
+	cout << "[" << bookWithMinQuantity<< "]\t" << left << setw (70) << format( "{}", bookTitle [bookWithMinQuantity]) << minQuantity << endl;
+	cout << "[" << bookWithMaxQuantity<< "]\t" << left << setw (70) << format( "{}", bookTitle [bookWithMaxQuantity]) << maxQuantity << endl;
 	
 	return 0;
 }
